226052e3ff02293f0d88bb16a4fee9a44222b9ea
BoooM0610 <eduardomartinezoc@gmail.com>
2022-06-13 14:34:16 +0200

    Initial commit

---
+++ src/main/java/com/cursoudemy/app/Proyecto6CursoUdemySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.cursoudemy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto6CursoUdemySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto6CursoUdemySpringBootApplication.class, args);
+	}
+
+}
